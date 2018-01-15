4d8e895fbc14e171e7d36653b2ed8ee7352fcc49
Murray Berkowitz <murray.berkowitz93@gmail.com>
2018-01-15 18:05:28 -0500

    gdf

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,9 @@
 import React, {Component} from 'react'
-import styles from '../../public/css/styles.css'
 
 export default class App extends Component {
     render(){
         return (
-            <h1 className={styles.test}>Welcome</h1>
+            <h1 className='test'>Welcome</h1>
         )
     }
 }
